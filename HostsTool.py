4051191c57dfb73c946f044d226f0d39cb3667bd
HaoBingo <devilhunterxx@qq.com>
2017-03-20 20:32:10 +0800

    first commit

---
+++ HostsTool.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#-*-coding:utf-8-*-
+
+import requests
+import os
+import time
+
+# https://github.com/racaljk/hosts
+hosts_url = "https://raw.githubusercontent.com/racaljk/hosts/master/hosts"
+
+systemroot = os.environ.get('SYSTEMROOT')
+host_file = os.path.join(systemroot,'system32\drivers\etc\hosts')
+host_file_bak = host_file + "_" + str(int(time.time()))+".bak"
+
+def down_host():
+    host_data = requests.get(hosts_url).content
+    with open(host_file,'w')as f:
+        f.write(host_data)
+    print "Hosts update successfully !"
+    
+def backup_host():
+    try:
+        os.rename(host_file, host_file_bak)
+        return True
+    except Exception:
+        print "Hosts update failed !"
+        print "Check %s" %(os.path.join(systemroot,'system32\drivers\etc'))
+        #print Exception
+        return False
+    
+def main():
+    if backup_host():
+        down_host()     
+
+if __name__ == "__main__":
+    main()
