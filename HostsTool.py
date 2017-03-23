1c03c4299d5f753d5f0393968dced0eb585f47d7
HaoBingo <devilhunterxx@qq.com>
2017-03-23 14:48:59 +0800

    Replace requests with urllib2

--- HostsTool.py
+++ HostsTool.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
 
-import requests
+#import requests
 import os
 import time
+import urllib2 
+
 
 # https://github.com/racaljk/hosts
 hosts_url = "https://raw.githubusercontent.com/racaljk/hosts/master/hosts"
@@ -13,10 +15,13 @@ host_file = os.path.join(systemroot,'system32\drivers\etc\hosts')
 host_file_bak = host_file + "_" + str(int(time.time()))+".bak"
 
 def down_host():
-    host_data = requests.get(hosts_url).content
+    #host_data = requests.get(hosts_url).content
+    host_data = urllib2.urlopen(hosts_url).read()
+    print host_data
     with open(host_file,'w')as f:
         f.write(host_data)
     print "Hosts update successfully !"
+    
     
 def backup_host():
     try:
@@ -34,3 +39,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    #down_host()
